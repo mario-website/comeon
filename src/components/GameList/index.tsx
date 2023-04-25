4c948825e616fd3d5bd3254685acc14c0001bbee
mario-website <mario.websie@gmail.com>
2023-04-25 12:47:53 +0200

    Resolved the issue where the game continued to run even after clicking the back button.

--- src/components/GameList/index.tsx
+++ src/components/GameList/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 
 // import "./style.scss";
 
@@ -9,18 +9,33 @@ const GameList = ({
   isGameRunning: boolean;
   setIsGameRunning: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const gameLaunchRef = useRef<HTMLDivElement | null>(null);
+
+  const removeGameLaunchChildren = () => {
+    if (gameLaunchRef.current) {
+      while (gameLaunchRef.current.firstChild) {
+        gameLaunchRef.current.removeChild(gameLaunchRef.current.firstChild);
+      }
+    }
+  };
+
+  const handleBackButton = () => {
+    setIsGameRunning(false);
+    removeGameLaunchChildren();
+  };
+
   return (
     <div className="ingame" style={{display: isGameRunning ? "block" : "none"}}>
       <div className="ui grid centered">
         <div className="three wide column">
           <div
             className="ui right floated secondary button inverted"
-            onClick={() => setIsGameRunning(false)}>
+            onClick={handleBackButton}>
             <i className="left chevron icon"></i>Back
           </div>
         </div>
         <div className="ten wide column">
-          <div id="game-launch"></div>
+          <div id="game-launch" ref={gameLaunchRef}></div>
         </div>
         <div className="three wide column"></div>
       </div>
